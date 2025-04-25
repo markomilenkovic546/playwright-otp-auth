9834ecbfefb6f87fe675140375540667baf08554
markomilenkovic546 <markomilenkovic546@gmail.com>
2025-04-25 02:13:44 +0200

    Create negative e2e tests to cover otp login flow with invalid and already used otp

--- tests/otp-login-flow.spec.ts
+++ tests/otp-login-flow.spec.ts
@@ -14,7 +14,7 @@ test.describe('OTP Login Flow', () => {
         await expect(page).toHaveURL(/otp-login/, { timeout: 10000 });
     });
 
-    test('Successful OTP login', async ({ page, loginPage, securePage }) => {
+    test('User should be able to log in with valid email and OTP', async ({ page, loginPage, securePage }) => {
         await test.step('Enter email in the email field', async () => {
             await loginPage.enterEmail(email);
             await expect(loginPage.emailField).toHaveValue(email);
@@ -35,6 +35,88 @@ test.describe('OTP Login Flow', () => {
             await loginPage.submitOtpToVerify();
             await expect(page).toHaveURL(/secure/, { timeout: 10000 });
             await expect(securePage.successMessage).toContainText('You logged into a secure area!');
+        });
+    });
+
+    test('User should not be able to log in with invalid OTP', async ({ page, loginPage, securePage }) => {
+        await test.step('Enter email in the email field', async () => {
+            await loginPage.enterEmail(email);
+            await expect(loginPage.emailField).toHaveValue(email);
+        });
+
+        await test.step('Submit email', async () => {
+            await loginPage.submitEmail();
+            await expect(loginPage.otpStatusMessage).toContainText("We've sent an OTP code to your email");
+        });
+
+        await test.step('Enter invalid OTP', async () => {
+            await loginPage.enterOtp('111111');
+            await expect(loginPage.otpField).toHaveValue('111111');
+        });
+
+        await test.step('Submit OTP', async () => {
+            await loginPage.submitOtpToVerify();
+            await expect(page).toHaveURL(/otp-login/, { timeout: 10000 });
+            await expect(loginPage.otpStatusMessage).toContainText(
+                'The provided OTP code is incorrect. Please check your code and try again.'
+            );
+        });
+    });
+
+    test('User should not be able to log in with already used OTP', async ({ page, loginPage, securePage }) => {
+        let otp: string;
+        await test.step('Enter email in the email field', async () => {
+            await loginPage.enterEmail(email);
+            await expect(loginPage.emailField).toHaveValue(email);
+        });
+
+        await test.step('Submit email', async () => {
+            await loginPage.submitEmail();
+            await expect(loginPage.otpStatusMessage).toContainText("We've sent an OTP code to your email");
+        });
+
+        await test.step('Get OTP from email and enter it', async () => {
+            const messageId = await emailHelper.getMessageId(email);
+            otp = await emailHelper.getOtp(email, messageId);
+            await loginPage.enterOtp(otp);
+        });
+
+        await test.step('Submit OTP', async () => {
+            await loginPage.submitOtpToVerify();
+            await expect(page).toHaveURL(/secure/, { timeout: 10000 });
+            await expect(securePage.successMessage).toContainText('You logged into a secure area!');
+        });
+
+        await test.step('Logout', async () => {
+            await securePage.logout();
+            await expect(page).toHaveURL(/login/, { timeout: 10000 });
+        });
+
+        await test.step('Open otp-login page', async () => {
+            await loginPage.open();
+            await expect(page).toHaveURL(/otp-login/, { timeout: 10000 });
+        });
+
+        await test.step('Enter email in the email field', async () => {
+            await loginPage.enterEmail(email);
+            await expect(loginPage.emailField).toHaveValue(email);
+        });
+
+        await test.step('Submit email', async () => {
+            await loginPage.submitEmail();
+            await expect(loginPage.otpStatusMessage).toContainText("We've sent an OTP code to your email");
+        });
+
+        await test.step('Enter already used OTP', async () => {
+            await loginPage.enterOtp(otp);
+        });
+
+        await test.step('Submit already used OTP', async () => {
+            await loginPage.submitOtpToVerify();
+            await expect(page).toHaveURL(/otp-login/, { timeout: 10000 });
+            await expect(loginPage.otpStatusMessage).toContainText(
+                'The provided OTP code is incorrect. Please check your code and try again.'
+            );
         });
     });
 });
